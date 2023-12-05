f1ef7f72a4e45a92e73ce7d990a306b0e0e766a4
Jesus Zerpa <jesus26abraham1996@gmail.com>
2023-12-04 23:09:56 -0400

    update

--- conversation_control/__init__.py
+++ conversation_control/__init__.py
@@ -255,7 +255,7 @@ class ConversationChain(ConversationChain):
         
         return result
     def __call__(self,*args,**kwargs):
-        print("vvvvvvvvvvvvvv",*args,**kwargs)
+        print("vvvvvvvvvvvvvv",args,kwargs)
         return super().__call__(*args,**kwargs)
 
     @classmethod
